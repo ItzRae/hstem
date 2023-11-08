8cb057d10973afef3ec5bbadc1abd05b5f586082
ItzRae <rachellin443@gmail.com>
2023-11-08 17:41:11 -0500

    relational schema implemented as django models

--- hstem-django-react/backend/hstem/models.py
+++ hstem-django-react/backend/hstem/models.py
@@ -2,9 +2,49 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(primary_key=True, max_length=100)
     major = models.CharField(max_length=50)
     year = models.IntegerField()
 
     def __str__(self):
         return self.name
+    
+class Project(models.Model):
+    title = models.CharField(primary_key=True, max_length=100)
+    description = models.CharField(max_length=1000)
+    primary_theme = models.CharField(max_length=100)
+    secondary_theme = models.CharField(max_length=100)
+    audience = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+class Create(models.Model):
+    date = models.DateField()
+    name = models.ForeignKey(Author, primary_key=True)
+    title = models.ForeignKey(Project, primary_key=True)
+
+    
+class Files(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    name = models.CharField(primary_key=True, max_length=100)
+    is_public = models.BooleanField()
+    type = models.CharField(max_length=50)
+    project = models.ForeignKey(Project, on_delete=models.DO_NOTHING) # on delete no action
+
+    def __str__(self):
+        return (self.title, self.name, self.type)
+    
+class Department(models.model):
+    school = models.CharField(primary_key=True, max_length=100)
+
+class Sponsor(models.Model):
+    school = models.CharField(primary_key=True, max_length=100)
+    title = models.CharField(primary_key=True, max_length=100)
+    departments = models.ForeignKey(Department)
+    project = models.ForeignKey(Project)
+
+    def __str__(self):
+        return (self.school, self.title)
+
+
