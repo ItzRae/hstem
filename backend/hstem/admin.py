67aa48273d14d5b4363f3f3009a9a73cf560a3e5
Nayeon Shin <dev.nayeon@gmail.com>
2023-12-12 17:22:11 -0500

    feat(backend): get all the data from DB

--- backend/hstem/admin.py
+++ backend/hstem/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Creates, Project
+from .models import Author, Creates, Project, File
 
 
 class AuthorAdmin(admin.ModelAdmin):
@@ -14,7 +14,11 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ("title", )
 
 
+class FileAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_public", "type", "uploaded_at", "file", "title")
+
+
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Creates, CreatesAdmin)
 admin.site.register(Project, ProjectAdmin)
-
+admin.site.register(File, FileAdmin)
