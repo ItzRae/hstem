cff3bb4c1c9acae690811fe7826554647c5e9977
Nayeon Shin <dev.nayeon@gmail.com>
2023-12-12 12:55:42 -0500

    feat(backend): remove hstem_author/

--- backend/hstem_author/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import HstemAuthor
-
-class HstemAuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'major', 'year')
-
-admin.site.register(HstemAuthor, HstemAuthorAdmin)
--- backend/hstem_author/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class HstemAuthor(models.Model):
-    name = models.CharField(primary_key=True, max_length=255)
-    major = models.CharField(max_length=255)
-    year = models.CharField(max_length=10)
-
-    class Meta:
-        db_table = 'hstem_author'
