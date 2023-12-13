d93c11df6964401e43ddd39da53dc852172684b4
Nayeon Shin <dev.nayeon@gmail.com>
2023-12-13 00:51:25 -0500

    chore: merge with previous PR

---
+++ frontend/src/components/MenuItem.js
@@ -0,0 +1,10 @@
+
+const MenuItem = ({ children, isLast, to = "/", ...rest }) => {
+    return (
+      <Link href={to}>
+        <Text display="block" {...rest}>
+          {children}
+        </Text>
+      </Link>
+    )
+  }
